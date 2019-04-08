747de6228bf6b4d2e8e68277bd2377851c34f364
Renato Violin <renato@MacBook-Pro-de-Renato.local>
2019-04-08 16:07:52 -0300

    predict on train script

---
+++ predict_on_train.sh
@@ -0,0 +1,22 @@
+export BERT_BASE_DIR=/home/renato/bert-final/uncased_L-12_H-768_A-12
+export SQUAD_DIR=/home/renato/bert-final/squad-min
+export OUTPUT_DIR=/home/renato/bert-final/output
+
+python 4L_squad.py \
+  --vocab_file=$BERT_BASE_DIR/vocab.txt \
+  --bert_config_file=$BERT_BASE_DIR/bert_config.json \
+  --init_checkpoint=$BERT_BASE_DIR/bert_model.ckpt \
+  --do_train=False \
+  --train_file=$SQUAD_DIR/train-v2.0-min.json \
+  --do_predict=True \
+  --predict_file=$SQUAD_DIR/train-v2.0-min.json \
+  --train_batch_size=12 \
+  --learning_rate=3e-5 \
+  --num_train_epochs=1.0 \
+  --max_seq_length=384 \
+  --doc_stride=128 \
+  --output_dir=$OUTPUT_DIR \
+  --version_2_with_negative=True
+
+TODAY=`date '+%H-%M-%S_%d-%m-%Y'`
+python evaluate-v2.0.py squad/dev-v2.0.json output/predictions.json > eval-train/$TODAY
