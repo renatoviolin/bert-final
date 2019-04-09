b76c72fac64baf7fb31722e82d3b457fa50fa7b9
Renato Violin <renato@MacBook-Pro-de-Renato.local>
2019-04-09 09:55:38 -0300

    small relu dropout 2

--- 3L_squad-small.py
+++ 3L_squad-small.py
@@ -646,8 +646,6 @@ def create_model(bert_config, is_training, input_ids, input_mask, segment_ids,
 
   logits = tf.matmul(logits, W3, transpose_b=True)
   logits = tf.nn.bias_add(logits, b3)
-  logits = tf.nn.relu(logits)
-  logits = tf.nn.dropout(logits, keep_prob)
 
   logits = tf.reshape(logits, [batch_size, seq_length, 2])
   logits = tf.transpose(logits, [2, 0, 1])
